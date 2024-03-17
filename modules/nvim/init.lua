8047c6f6bcfb11b0872ea7f5c5b96d0bbd6bb7f7
notsooSleepy <bartlomiej.grabarek1@gmail.com>
2024-03-17 21:08:01 +0100

    chase

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -1,4 +1,4 @@
---[[
+  --[[
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -820,7 +820,20 @@ require("lazy").setup({
 			--  Check out: https://github.com/echasnovski/mini.nvim
 		end,
 	},
-
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
