24ac1ddac6eb3945bf05db3bc94e858920773ce2
notsooSleepy <bartlomiej.grabarek1@gmail.com>
2024-07-02 20:53:48 +0200

    flake update

--- flake.nix
+++ flake.nix
@@ -3,23 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "github:hyprwm/Hyprland";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }: {
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
       nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs; };
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [ 
           ./desktop.nix
           inputs.home-manager.nixosModules.default
-          hyprland.homeManagerModules.default
-          {
-            wayland.windowManager.hyprland.enable = true;
-          }
         ];
       };
       nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
@@ -28,10 +24,6 @@
         modules = [ 
           ./laptop.nix
           inputs.home-manager.nixosModules.default
-          hyprland.homeManagerModules.default
-          {
-            wayland.windowManager.hyprland.enable = true;
-          }
         ];
       };
     };
