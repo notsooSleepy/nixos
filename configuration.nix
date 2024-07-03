46572eb9977bfa206c570e2dcf86c7432173ea01
notsooSleepy <bartlomiej.grabarek1@gmail.com>
2024-07-03 20:42:47 +0200

    unstable cachix for hyprland

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,11 @@
     ];
   # flake setup
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # cachix for hyprland
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
   # for lua-language-server
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
