08483d77d4d63ca29f31492210c878f9b4556573
notsooSleepy <bartlomiej.grabarek1@gmail.com>
2024-12-14 21:50:55 +0100

    sorted app in configuration.nix and home.nix

--- configuration.nix
+++ configuration.nix
@@ -14,13 +14,6 @@
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
-  # for lua-language-server
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    lua-language-server
-    # Add any missing dynamic libraries for unpackaged programs
-    # here, NOT in environment.systemPackages
-  ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -43,14 +36,27 @@
   };
   # Configure console keymap
   console.keyMap = "pl2";
-  # livebook ---------------------------
-    # services.livebook = {
-    #   enableUserService = true;
-    #   environmentFile = pkgs.writeText "livebook.env" ''
-    #     LIVEBOOK_PORT = 20123
-    #     LIVEBOOK_PASSWORD = "1234";
-    #   '';
-    # };
+  # blueman for wifi
+  services.blueman.enable = true;
+  # audio
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = false;
+  # Bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  # unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnsupportedSystem = true;
+  #shell
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  # hyprland
+  security.polkit.enable = true;
+  security.pam.services.swaylock = {};
+  programs.hyprland = { 
+  enable = true;
+  xwayland.enable = true;
+  };
   # ------------------------------------
   # display manager
   services = {
@@ -63,20 +69,6 @@
       };
     };
   };
-  #shell
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  # Bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  # hyprland
-  security.polkit.enable = true;
-  security.pam.services.swaylock = {};
-  programs.hyprland = { 
-  enable = true;
-  xwayland.enable = true;
-  # package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  };
   # thunar
   programs.thunar = {
     enable = true;
@@ -87,12 +79,6 @@
   };
   services.gvfs.enable = true;
   services.tumbler.enable = true;
-  # blueman for wifi
-  services.blueman.enable = true;
-  # audio
-  # sound.enable = true;
-  nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio.enable = false;
   # flatpack
   services.flatpak.enable = true;
   systemd.services.flatpak-repo = {
@@ -102,12 +88,19 @@
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
+  # ------------------------------------
+  # docker
+  virtualisation.docker.enable = true;
+  # for lua-language-server
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    lua-language-server
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
   # wacom tablet
   hardware.opentabletdriver.enable = true;
   hardware.opentabletdriver.daemon.enable = true;
-  # unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnsupportedSystem = true;
   # steam
   programs.steam = {
     enable = true;
@@ -115,20 +108,6 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
 };
-  # postgresql
-  services.postgresql = {
-    enable = true;
-    initialScript = pkgs.writeText "init.sql" ''
-      CREATE USER phoenix_user WITH PASSWORD 'postgres';
-      CREATE DATABASE phoenix_dev OWNER phoenix_user;
-      CREATE DATABASE phoenix_test OWNER phoenix_user;
-    '';
-    authentication = ''
-      local   all             postgres                                peer
-      host    all             all             127.0.0.1/32            md5
-      host    all             all             ::1/128                 md5
-    '';
-  };
   # fonts
   fonts.packages = with pkgs; [
   nerdfonts
@@ -138,49 +117,74 @@
   users.users.sleepy = {
     isNormalUser = true;
     description = "sleepy";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
+  # ------------------------------------
     gcc
     gnumake
+  # ---------------
+    pavucontrol
+    wireplumber
+  # ---------------
+    wlogout
+    swayidle
+    swaylock
+  # ---------------
+    networkmanagerapplet
+    libappindicator-gtk3
+    inotify-tools
+  # ---------------
+    fira-code
+  # ------------------------------------
     git
     gh
-    google-chrome
-    vivaldi
-    kitty
-    rofi-wayland
-    pavucontrol
     lf
     du-dust
     btop
     thefuck
     grimblast
-    obsidian
+    ripgrep
+    bat
+    fd
+    wl-clipboard
+    wget
+    unzip
+    usermount
+  # ------------------------------------
+    google-chrome
+    vivaldi
+    kitty
     qbittorrent
-    swww
-    fira-code
-    mpv-unwrapped
-    ani-cli
-    lua-language-server
-    networkmanagerapplet
-    libappindicator-gtk3
-    wlogout
-    swayidle
-    swaylock
-    wireplumber
-    livebook
     mate.atril
-    inotify-tools
+  # ------------------------------------
+    rofi-wayland
+    swww
+    feh
+  # ------------------------------------
+  # ------------------------------------
+    rustup
+    cargo
+  # ---------------
     python3
     python313Full
     python312Packages.pytest
     python312Packages.pip
+  # ---------------
+    elixir_1_17
+    erlang_27
+  # ---------------
     nodejs_22
-    go
-    tree-sitter
-    cargo
+    nodePackages_latest.node-red
+  # ---------------
     lua
-    jdk
-    jdk11
+    lua-language-server
+    luajitPackages.luarocks
+    tree-sitter
+  # ---------------
+    go
+  # ---------------
+    htmx-lsp
+  # ------------------------------------
     ];
   }; 
   # home manager
@@ -193,7 +197,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    postgresql
     wget
     git
     libsForQt5.qt5.qtgraphicaleffects
